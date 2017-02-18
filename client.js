8b42757d486b5b615a14336cebc85d4f05ab0beb
Gina Lolli <ginalolli@gmail.com>
2017-02-18 18:22:40 +0100

    add version 1 requirements

---
+++ client.js
@@ -0,0 +1,5 @@
+// it should have a way to store todos
+// it should have a way to display todos
+// it should have a way to add todos
+// it should have a way to change todos
+// it should have a way to delete todos
