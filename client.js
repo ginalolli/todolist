6688fe12fed06016d06118e326120498d11c2c36
Gina Lolli <ginalolli@gmail.com>
2017-02-18 18:54:39 +0100

    create todolist object + create methods

--- client.js
+++ client.js
@@ -1,25 +1,28 @@
-// it should have a way to store todos
-var todos = ['item 1', 'item 2', 'item 3'];
+// it should store the todos arrays on an object
 
-// it should have a fucnction to display todos
-function displayTodos() {
-  console.log("My todos", todos);
-}
+todosList = {
+  todos: ['item 1', 'item 2', 'item 3'],
 
-// it should have a way to add todos
-function addTodos(todo) {
-  todos.push(todo);
-  displayTodos();
-}
+// it should have a display todos method
+  displayTodos: function() {
+    console.log("My todos", this.todos); //"this" refers to entire todosList object
+  },
 
-// it should have a way to change todos
-function changeTodo(position, newValue) {
-  todos[position] = newValue;
-  displayTodos();
-}
+  // it should have an add todo method
+  addTodo: function(todo) {
+    this.todos.push(todo);
+    this.displayTodos();
+  },
 
-// it should have a way to delete todos
-function deleteTodo(position) {
-  todos.splice(position, 1);
-  displayTodos();
+  //it should have a change todo method
+  changeTodo: function(position, newValue) {
+    this.todos[position] = newValue;
+    this.displayTodos();
+  },
+
+  // it should have a delete todo method
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  }
 }
