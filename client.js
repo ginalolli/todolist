0ba821712516781059e4e6dc5d07f5bd4c9401da
Gina Lolli <ginalolli@gmail.com>
2017-02-18 19:10:55 +0100

    addtodo add objects, change todotext property, toggle completed changed completed property

--- client.js
+++ client.js
@@ -8,21 +8,31 @@ todosList = {
     console.log("My todos", this.todos); //"this" refers to entire todosList object
   },
 
-  // it should have an add todo method
-  addTodo: function(todo) {
-    this.todos.push(todo);
+  // addTodo should add objects ie todo text
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText, // property: parameter
+      completed: false;
+    });
     this.displayTodos();
   },
 
-  //it should have a change todo method
-  changeTodo: function(position, newValue) {
-    this.todos[position] = newValue;
+  //chnage todo should change the todo text property
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText; //todolistObject.todosArray[positionParameter].todoTextProperty = todoTextValue
     this.displayTodos();
   },
 
   // it should have a delete todo method
   deleteTodo: function(position) {
     this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+
+  //toggle Completed should flip the completed property
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
     this.displayTodos();
   }
 }
