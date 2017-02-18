9d1af3a0d3602ae2a71014df0519e1e9ce4efb86
Gina Lolli <ginalolli@gmail.com>
2017-02-18 18:37:36 +0100

    declaring todo list basic functions

--- client.js
+++ client.js
@@ -1,5 +1,25 @@
 // it should have a way to store todos
-// it should have a way to display todos
+var todos = ['item 1', 'item 2', 'item 3'];
+
+// it should have a fucnction to display todos
+function displayTodos() {
+  console.log("My todos", todos);
+}
+
 // it should have a way to add todos
+function addTodos(todo) {
+  todos.push(todo);
+  displayTodos();
+}
+
 // it should have a way to change todos
+function changeTodo(position, newValue) {
+  todos[position] = newValue;
+  displayTodos();
+}
+
 // it should have a way to delete todos
+function deleteTodo(position) {
+  todos.splice(position, 1);
+  displayTodos();
+}
