9f42bae9a2b67cb80bceaa1f54b872e420c5da17
Gina Lolli <ginalolli@gmail.com>
2017-02-19 15:48:33 +0100

    add delete buttons after each todolist item

--- script.js
+++ script.js
@@ -65,10 +65,8 @@ var handlers = {
     changeTodoTextInput.value = '';
     view.displayTodos();
   },
-  deleteTodo: function() {
-    var deleteTodoPositionInput = document.getElementById('deleteTodoPositionInput');
-    todoList.deleteTodo(deleteTodoPositionInput.valueAsNumber);
-    deleteTodoPositionInput.value = '';
+  deleteTodo: function(position) {
+    todoList.deleteTodo(position);
     view.displayTodos();
   },
   toggleCompleted: function() {
@@ -102,8 +100,31 @@ var view = {
          todoTextWithCompletion = "( ) " + todo.todoText;
        }
 
+      todoLi.id = i;
       todoLi.textContent = todoTextWithCompletion;
+      todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
     }
+  },
+  createDeleteButton: function() {
+    var deleteButton = document.createElement("button");
+    deleteButton.textContent = "Delete";
+    deleteButton.className = "deleteButton";
+    return deleteButton;
+  },
+  setUpEventListeners: function() {
+    var todosUl = document.querySelector('ul');
+    todosUl.addEventListener('click', function(event) {
+    //console.log(event.target.parentNode.id);
+    //get the element that was clicked on
+    var elementClicked = event.target;
+
+      //check if element clicked is delete button
+      if (elementClicked.className === 'deleteButton') {
+        handlers.deleteTodo(parseInt(elementClicked.parentNode.id)); // parseInt - this gets the id of button as a number , passes it in as "position" arg
+      }
+    });
   }
 };
+
+view.setUpEventListeners();
