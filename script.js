1cd0a29e6173f9877624c11cde51a4ef66412042
Gina Lolli <ginalolli@gmail.com>
2017-02-21 08:43:55 +0100

    update view object to use forEach

--- script.js
+++ script.js
@@ -85,25 +85,24 @@ var handlers = {
 // this just shows what the todo list looks like
 var view = {
   displayTodos: function() {
-     var todosUl = document.querySelector('ul');
-     todosUl.innerHTML = '';
+    var todosUl = document.querySelector('ul');
+    todosUl.innerHTML = '';
 
-    for (var i = 0; i < todoList.todos.length; i++) {
+    todoList.todos.forEach( function(todo, position) {
       var todoLi = document.createElement('li');
-      var todo = todoList.todos[i];
       var todoTextWithCompletion = '';
 
-       if (todo.completed === true) {
-         todoTextWithCompletion = "(x) " + todo.todoText;
-       } else {
-         todoTextWithCompletion = "( ) " + todo.todoText;
-       }
+      if (todo.completed === true) {
+        todoTextWithCompletion = "(x) " + todo.todoText;
+      } else {
+        todoTextWithCompletion = "( ) " + todo.todoText;
+      }
 
-      todoLi.id = i;
+      todoLi.id = position;
       todoLi.textContent = todoTextWithCompletion;
       todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
-    }
+    } ,this); // forEach(callback, this) [callback is equivalent to callback fn in line 107]
   },
   createDeleteButton: function() {
     var deleteButton = document.createElement("button");
